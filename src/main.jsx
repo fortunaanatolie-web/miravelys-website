6f3d145856ec91a4ee4dcdbbf6f4c8e3c956c716
fortunaanatolie-web <fortunaanatolie@gmail.com>
2026-09-01 16:03:16 +0300

    Load final MiraScribe recovery polish

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,7 @@ import './styles/site-mirascribe.css';
 import './styles/site-mirascribe-marketing.css';
 import './styles/site-mirascribe-marketing-fixes.css';
 import './styles/site-mirascribe-artwork-fixes.css';
+import './styles/site-mirascribe-recovery-tweaks.css';
 import SiteRouter from './SiteRouter';
 
 document.documentElement.classList.add('js');
